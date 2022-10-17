0bc3f9e4e7116369334ca0599faf74afb81a4c33
xuzou <xzou42@wisc.edu>
2022-10-17 11:05:15 -0500

    add scripts

--- school.sh
+++ school.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv|grep "MADISON SCHOOLS"|cut -d , -f7 |{ sum=0; num=0; while read n; do sum=$(($sum+$n)); num=$(($num+1)); done; echo sum=$sum,average=$(($sum/$num));}
