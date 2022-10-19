58e97029798c4a2680c024a96a7f22a6ca18b3f2
xuzou <xzou42@wisc.edu>
2022-10-19 12:34:35 -0500

    adjust the format

--- school.sh
+++ school.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-cat Property_Tax_Roll.csv|grep "MADISON SCHOOLS"|cut -d , -f7 |{ sum=0; num=0; while read n; do sum=$(($sum+$n)); num=$(($num+1)); done; echo sum=$sum,average=$(($sum/$num));}
+cat Property_Tax_Roll.csv|
+    grep "MADISON SCHOOLS"|
+    cut -d , -f7 |
+    { sum=0;
+      num=0;
+      while read n;do
+	  sum=$(($sum+$n));
+	  num=$(($num+1));
+      done;
+      echo sum=$sum,average=$(($sum/$num));}
