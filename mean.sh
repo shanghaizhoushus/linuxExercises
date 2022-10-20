77c009481452b6a8d79c67a01fde066cc202467b
Qizhou Huang <qhuang96@wisc.edu>
2022-10-19 23:31:57 -0500

    Mean.sh is written, but I'm not sure if it is correct.

--- mean.sh
+++ mean.sh
@@ -1,1 +1,21 @@
-#!/bin/bash
+#!/bin/bash                                                                     
+
+if [[ $# -ne 2 ]]; then
+    echo "usage: $0 <dir> <n>" 1>&2
+    exit 0
+fi
+
+col=$1
+data=$2
+
+
+cat $data |
+cut -d , -f $col |
+tail -n +2 |
+{ sum=0;
+      num=0;
+      while read n;do
+          sum=$(($sum+$n));
+          num=$(($num+1));
+      done;
+      echo average=$(($sum/$num));}
