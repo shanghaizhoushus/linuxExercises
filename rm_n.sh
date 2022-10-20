d4088e66e2f4d100b64ab22313684d846f7901df
xuzou <xzou42@wisc.edu>
2022-10-19 20:37:24 -0500

    add scripts

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo "usage: $0 <dir> <n>" 1>&2
+    exit 0
+fi
+
+dir=$1
+n=$2
+
+for file in $(find $dir -type f -size +$n"c"); do
+    rm -r $file
+done
+
