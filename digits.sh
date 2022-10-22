c9ca07e95d6331d9931f0a2246da582d8c83086d
xuzou <xzou42@wisc.edu>
2022-10-21 19:09:51 -0500

    modify code

--- digits.sh
+++ digits.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 k=0
-for i in 1{1,0}{1,0}{1,0};
-do k=$(($k+$i));
+
+for i in 1{1,0}{1,0}{1,0};do
+    k=$(($k+$i));
 done
+
 echo $k
--- mean.sh
+++ mean.sh
@@ -1,18 +1,21 @@
-#!/bin/bash                                                                     
+#!/bin/bash
 
-if [[ $# -ne 2 ]]; then
-    echo "usage: $0 <dir> <n>" 1>&2
+if [[ $# -eq 1 ]]; then
+    col=$1
+    file=/dev/stdin
+elif [[ $# -eq 2 ]]; then
+    col=$1
+    file=$2
+else
+    echo "usage: $0 <column> [file.csv]" 1>&2
     exit 0
 fi
 
-col=$1
-data=$2
 
-
-cat $data |
-cut -d , -f $col |
-tail -n +2 |
-{ sum=0;
+cat $file |
+    cut -d , -f $col |
+    tail -n +2 |
+    { sum=0;
       num=0;
       while read n;do
           sum=$(($sum+$n));
